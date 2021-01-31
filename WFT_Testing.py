e7d718f9bc5722d3bc520b9628e3a35f794e2619
robarca <roberto.arcamone@studio.unibo.it>
2021-01-31 09:34:35 +0100

    update of new tests

--- WFT_Testing.py
+++ WFT_Testing.py
@@ -12,7 +12,7 @@ from hypothesis import given
 
 # Test for the function "z_vector"
 
-@given(st.integers(0,49),st.floats(0.005,1),st.integers(1,50))
+@given(st.integers(0,49),st.floats(0.005,1),st.integers(50,100))
 @settings(max_examples = 10)
 
 def test_z_vector(z1,dz,z2):
@@ -27,9 +27,19 @@ def test_z_vector(z1,dz,z2):
     with pytest.raises(ValueError):
        fn.z_vector(10,0.05,5)
        
+    #check if a ValueError arises if dz in negative
+    
+    with pytest.raises(ValueError):
+       fn.z_vector(1,-0.05,5)
+    
+    #check if a ValueError arises if dz is not in the right range
+    
+    with pytest.raises(ValueError):
+       fn.z_vector(1,10,5)
     #check if the z output vector exists
     
     assert(len(z) >= 1)
+    
     
 # Test for the function "normalized_density_profile"  
         
@@ -45,8 +55,13 @@ def test_normalized_density_profile(z1,dz,z2,h):
     #check if a ValueError arises if h is negative
      
     with pytest.raises(ValueError):
-       fn.normalized_density_profile(z,-1) #check z al posto di np.appary come dopo
-       
+       fn.normalized_density_profile(z,-1) 
+    
+    #check decreasing monotonicity of rho_n profile
+ 
+    for k in range(0, len(rho_n)-2):
+    	assert(rho_n[k] >= rho_n[k+1])
+    	 	   
     #check if a ValueError arises if h is 0
     
     with pytest.raises(ValueError):
@@ -54,47 +69,48 @@ def test_normalized_density_profile(z1,dz,z2,h):
     
     #check if the rho_n output vector exists
     
-    assert(len(rho_n) >= 1)
-       
+    assert(len(rho_n) >= 1)    
+
 # Test for the function "optical_depth" and "TOA_transmittances"
 
-@given(st.integers(0,49),st.floats(0.005,1),st.integers(1,50),st.floats(1,10),st.integers(0,49),st.integers(1,50),st.floats(0.001,0.5),st.floats(1,5))
+@given(st.integers(0,20),st.integers(51,100),st.floats(1,10),st.integers(20,30),st.integers(31,50),st.floats(0.001,0.5),st.floats(1,5))
 @settings(max_examples = 10)
 
-def test_optical_depth_and_transmittances(z1,dz,z2,h,b,t,csg,coc):
+def test_optical_depth_and_transmittances(z1,z2,h,b,t,csg,coc):
+    
+    dz=0.5
     
     z = fn.z_vector(z1,dz,z2)
       
     rho_n = fn.normalized_density_profile(z,h)
       
-    lod,loc=fn.optical_depth(z,dz,b,t,csg,coc,rho_n)
-      
     #check if b (cloud's base) and t (cloud's top) are positive 
       
     assert(b > 0)
     assert(t > 0)
-      
+    
+    #check if b is bigger than z1 and t is smaller then z2
+   
+    assert(b >= z1)
+    assert(z2 >= t)
+        
     #check is t is bigger then b
       
     assert(t > b)
     
     #check if coc and csg are positive
     
+    lod,loc=fn.optical_depth(z,dz,b,t,csg,coc,rho_n)
     assert(coc > 0)
     assert(csg > 0)
       
     #check if a ValueError arises if b and t are negative 
     
     with pytest.raises(ValueError):
-       fn.optical_depth(z,0.005,-1,15,2,0.2,2,rho_n) #np.array([1]) ->z e rho_n
+       fn.optical_depth(z,0.005,-1,15,2,0.2,rho_n) 
        
     with pytest.raises(ValueError):
-       fn.optical_depth(z,0.005,10,-1,2,0.2,2,rho_n)  
-       
-    #check if a ValueError arises if one of the coefficients (cos or csg) is negative 
-    
-    with pytest.raises(ValueError):
-       fn.optical_depth(z,0.005,10,8,12,-0.2,2,rho_n)  
+       fn.optical_depth(z,0.005,10,-1,2,0.2,rho_n)  
        
     #check if the lod e loc output vector exist
     
@@ -107,4 +123,31 @@ def test_optical_depth_and_transmittances(z1,dz,z2,h,b,t,csg,coc):
     
     assert(len(clear_t) >= 1)
     assert(len(cloudy_t) >= 1)
+
+# Test for the function "weighting function"
+
+@given(st.integers(0,20),st.integers(51,100),st.floats(1,10),st.integers(20,30),st.integers(31,50))
+@settings(max_examples = 10)
+
+def test_weighting_functions(z1,z2,h,b,t):
+
+    dz=0.5
     
+    csg=0.2
+    
+    coc=2
+    
+    z = fn.z_vector(z1,dz,z2)
+      
+    rho_n = fn.normalized_density_profile(z,h)
+    
+    lod,loc=fn.optical_depth(z,dz,b,t,csg,coc,rho_n)
+    
+    clear_t, cloudy_t = fn.TOA_transmittances(lod, loc, z)
+    
+    weight_clear, weight_cloudy = fn.weighting_function(clear_t, cloudy_t, z, dz)
+    
+    #check if the weight_clear and weight_cloudy output vectors exist
+    
+    assert(len(weight_clear) >= 1)
+    assert(len(weight_cloudy) >= 1)
